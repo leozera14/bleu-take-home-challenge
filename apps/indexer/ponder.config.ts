fac2cf9b90611dd3767bc0d6565a8bf914c2a753
leozera <leo_faleiros@hotmail.com>
2025-06-23 00:16:29 -0300

    fix: Pushing fix to GitHub deploy error on ponder.config.ts

--- apps/indexer/ponder.config.ts
+++ apps/indexer/ponder.config.ts
@@ -4,24 +4,36 @@ import { http } from 'viem';
 
 import {BleuNFTAbi, BleuNFTStakerAbi} from "./abis/index"
 
+function getEnv(name: string): string {
+  const varValue = process.env[name];
+
+  if (!varValue) throw new Error(`Missing env var: ${name}`);
+  
+  return varValue;
+}
+
+const RPC_URL = getEnv("RPC_URL");
+const NFT_CONTRACT_ADDRESS = getEnv("NFT_CONTRACT_ADDRESS") as `0x${string}`;
+const STAKER_CONTRACT_ADDRESS = getEnv("STAKER_CONTRACT_ADDRESS") as `0x${string}`;
+
 export default createConfig({
   networks: {
     sepolia: {
       chainId: 11155111,
-      transport: http(process.env.RPC_URL! as string),       
+      transport: http(RPC_URL),       
     },
   },
   contracts: {
     BleuNFT: {
       abi: BleuNFTAbi,
       network: "sepolia",
-      address: process.env.NFT_CONTRACT_ADDRESS! as `0x${string}`,  
+      address: NFT_CONTRACT_ADDRESS,  
       startBlock: 0,                              
     },
     BleuNFTStaker: {
       abi: BleuNFTStakerAbi,
       network: "sepolia",
-      address: process.env.STAKER_CONTRACT_ADDRESS! as `0x${string}`, 
+      address: STAKER_CONTRACT_ADDRESS, 
       startBlock: 0,
     },
   },
--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,7 +1,5 @@
-import { IHandleUseQuery } from '@/types/handleUserQuery';
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
-import { useQuery } from 'urql';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
